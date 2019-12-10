8b8f0775e43de2460c550cde8cfc6bb8aa45e305
Dave Betts <ibedave777@gmail.com>
2019-12-10 15:40:15 -0500

    Working on it.... lot of changes, not working yet

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const NumberButton = () => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button onClick={ () => {
+      props.set(props.total + props.button)
+     
+    }}
+    
+      className='calc-button number-button'>{props.button}</button>
   );
 };
+
+export default NumberButton;
